343dc680a78a0ec0a1f89988f924897f3169214c
Kas K <>
2021-05-04 21:18:04 +0000

    Added calculations. Fixed tests.

--- src/exercise.py
+++ src/exercise.py
@@ -2,6 +2,9 @@ def main():
     #write your code below this line
     # Read the numbers b and h like this:
     b = int(input())
+    h = int(input())
+    area = 0.5*b*h
+    print(str(area))
 
 if __name__ == '__main__':
     main()
--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -30,4 +30,7 @@ def test_exercise():
     src.exercise.print = lambda s : output.append(s)
     src.exercise.main()
     
-    assert [output[0],output[1],output[2],output[3]] == ["7.5","10.0","137293.0","43975.5"]
+    assert [output[0],output[1],output[2]] == ["","","7.5"]
+    assert [output[3],output[4],output[5]] == ["","","10.0"]
+    assert [output[6],output[7],output[8]] == ["","","137293.0"]
+    assert [output[9],output[10],output[11]] == ["","","43975.5"]
